b4c022679bdb3c5621d21fd1a99ae76e3f5ee9e1
Ferico Samuel <ferico55@gmail.com>
2017-11-05 10:44:04 +0700

    boilerplate

--- app/src/main/java/id/d_amigos/rxsample/MainActivity.java
+++ app/src/main/java/id/d_amigos/rxsample/MainActivity.java
@@ -2,6 +2,17 @@ package id.d_amigos.rxsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.jakewharton.rxbinding2.widget.TextViewAfterTextChangeEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
