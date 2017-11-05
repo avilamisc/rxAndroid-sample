42648a0bb24139c7eb30f31d8896c9b0f85926cd
Ferico Samuel <ferico55@gmail.com>
2017-11-05 18:19:18 +0700

    sample

--- app/src/main/java/id/d_amigos/rxsample/MainActivity.java
+++ app/src/main/java/id/d_amigos/rxsample/MainActivity.java
@@ -3,7 +3,10 @@ package id.d_amigos.rxsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.jakewharton.rxbinding2.widget.TextViewAfterTextChangeEvent;
@@ -14,11 +17,37 @@ import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView lblHello;
+    private EditText txtSearch;
+    private Button btn1, btn2, btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtSearch = (EditText) findViewById(R.id.txtSearch);
+        lblHello = (TextView) findViewById(R.id.lblHello);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int event = 0;
+        if (view.getId() == btn1.getId()) {
+            event = 1;
+        } else if (view.getId() == btn2.getId()) {
+            event = 2;
+        } else {
+            event = 3;
+        }
     }
 }
